14a618dcf7f5391b421036bb23353916022ab074
Ryan <ryanfrost2015@gmail.com>
2020-01-15 17:08:42 +0000

    TobaccoStall implements ISecurity

--- src/main/java/stalls/TobaccoStall.java
+++ src/main/java/stalls/TobaccoStall.java
@@ -3,7 +3,7 @@ package stalls;
 import behaviours.ISecurity;
 import people.Visitor;
 
-public class TobaccoStall extends Stall {
+public class TobaccoStall extends Stall implements ISecurity {
 
     public TobaccoStall(String name, String ownerName, ParkingSpot parkingSpot, int rating) {
         super(name, ownerName, parkingSpot, rating);
