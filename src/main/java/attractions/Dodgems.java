4dc7ed6d88c5c0ace86af37177045f46f3118574
Ryan <ryanfrost2015@gmail.com>
2020-01-15 17:15:43 +0000

    Dodgems's default price is 4.50

--- src/main/java/attractions/Dodgems.java
+++ src/main/java/attractions/Dodgems.java
@@ -9,7 +9,7 @@ public class Dodgems extends Attraction implements ITicketed {
 
     public Dodgems(String name, int rating) {
         super(name, rating);
-        this.price = 4.00;
+        this.price = 4.50;
     }
 
     public double defaultPrice(){ return this.price; }
